78081af3d6b399e746f9b8bf48883870f4c4c48e
Michael Madela <michael.madela@hotmail.de>
2021-07-29 15:23:52 +0200

    Documentation on what to use to improve the tool added.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,7 +3,7 @@ import datetime
 import pandas as pd
 import numpy as np
 """
-Next project is to improve this tool.
+Next project is to improve this tool using the Udacity feedback and create an app.
 """
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
